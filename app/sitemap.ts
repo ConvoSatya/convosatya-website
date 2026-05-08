f21dc8141e1caecd1498182d1010d60b2f7f5d27
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2026-05-08 11:57:56 -0400

    Fixed sitemap Conflict
    There were two

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,32 @@
-import { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://www.convosatya.com";
+
   return [
     {
-      url: 'https://convosatya.com',
+      url: `${baseUrl}/`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: 'https://convosatya.com/platform',
+      url: `${baseUrl}/faq`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.8,
     },
-  ]
+    {
+      url: `${baseUrl}/faust-demo`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/platform`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+  ];
 }
